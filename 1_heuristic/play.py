dc50d81d07279fc90d29550ed840ae4f8b5e75d3
apache2046 <gaohaibin@bantus.cn>
2022-06-11 16:53:51 +0800

    1

--- 1_heuristic/play.py
+++ 1_heuristic/play.py
@@ -1,18 +1,24 @@
 import sys
 import os
 import time
+
 sys.path.append(os.path.dirname(__file__) + "/../")
+sys.path.append(os.path.dirname(__file__))
 
 from back_end import serv
 import numpy as np
+import evaluate as eva
 
-board_state = np.zeros((15,15))
+board_state = np.zeros((15, 15))
+
+
 def board_size(size):
     global board_state
     print("in play board_size", size, id(board_state))
-    board_state = np.zeros((size,size), dtype=np.int32)
+    board_state = np.zeros((size, size), dtype=np.int32)
     print("in play board_size2", board_state.shape, id(board_state))
     return True
+
 
 def clearboard():
     global board_state
@@ -21,15 +27,17 @@ def clearboard():
     print("play clearboard2")
     return True
 
+
 def play(actor, pos):
-    print('in play play', actor, pos)
+    print("in play play", actor, pos)
     x, y = pos
-    
+
     board_state[y][x] = actor
     return True
 
+
 def genmove(actor):
-    print('in play genmove', board_state.shape)
+    print("in play genmove", board_state.shape)
     size = board_state.shape[0]
     time.sleep(2)
     while True:
