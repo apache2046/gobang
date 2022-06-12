0ffa915c2c2b5878bdac3c137f03bc985b98c221
apache2046 <gaohaibin@bantus.cn>
2022-06-12 23:13:17 +0800

    format

--- back_end/__init__.py
+++ back_end/__init__.py
@@ -1,58 +1,59 @@
 from flask import Flask, request, g, abort, jsonify, render_template, redirect
 import os
-__all__ = [
-    "serv"
-]
+
+__all__ = ["serv"]
+
 
 def serv(port=8080, boardsize_cb=None, clearboard_cb=None, play_cb=None, genmove_cb=None):
     static_dir = os.path.dirname(__file__) + "/../front_end/dist"
     print(static_dir)
-    app = Flask('go_srv', static_folder = static_dir, static_url_path="/")
+    app = Flask("go_srv", static_folder=static_dir, static_url_path="/")
 
-    @app.route('/')
+    @app.route("/")
     def index():
-        return redirect('/index.html')
+        return redirect("/index.html")
 
-    @app.route('/boardsize', methods=['GET', 'POST'])
+    @app.route("/boardsize", methods=["GET", "POST"])
     def boardsize():
-        print('boardsize_cb')
+        print("boardsize_cb")
         params = request.json
         if boardsize_cb is not None:
             boardsize_cb(params["size"])
-        return jsonify({'status':'ok'})
-    
-    @app.route('/clearboard', methods=['GET', 'POST'])
+        return jsonify({"status": "ok"})
+
+    @app.route("/clearboard", methods=["GET", "POST"])
     def clearboard():
-        print('clearboard')
+        print("clearboard")
         # params = request.json
         if clearboard_cb is not None:
             clearboard_cb()
-        return jsonify({'status':'ok'})
+        return jsonify({"status": "ok"})
 
-    @app.route('/play', methods=['POST'])
+    @app.route("/play", methods=["POST"])
     def play():
-        print('play')
+        print("play")
         params = request.json
         ret = True
         if play_cb is not None:
             ret = play_cb(params["actor"], params["pos"])
-    
+
         if ret:
-            return jsonify({'status':'ok'})
+            return jsonify({"status": "ok"})
         else:
-            return jsonify({'status':'failed'})
-    
-    @app.route('/genmove', methods=['POST'])
+            return jsonify({"status": "failed"})
+
+    @app.route("/genmove", methods=["POST"])
     def genmove():
-        print('genmove')
+        print("genmove")
         params = request.json
         if genmove_cb is not None:
             ret = genmove_cb(params["actor"])
-            print('genmove2 ', ret, type(ret), type(ret[1]))
-            return jsonify({'status':'ok', 'pos': ret})
-        return jsonify({'status':'failed'})
-    
+            print("genmove2 ", ret, type(ret), type(ret[1]))
+            return jsonify({"status": "ok", "pos": ret})
+        return jsonify({"status": "failed"})
+
     app.run(host="0.0.0.0", port=port, debug=True, threaded=True, processes=1)
+
 
 if __name__ == "__main__":
     serv()
